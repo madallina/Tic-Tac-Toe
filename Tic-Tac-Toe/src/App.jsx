3e6346916b31a325851225b574a8a422c138dbd6
madallina <goncearucmadalina@yahoo.com>
2024-10-17 12:56:11 +0300

    refactor: creating a reusable component Square

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,21 +1,25 @@
 
-export default function Square(){
+function Square(){
+  return <button className="square">X</button>;
+}
+
+export default function Board(){
   return(
     <>
     <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <Square/>
+      <Square/>
+      <Square/>
      </div>
     <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <Square/>
+      <Square/>
+      <Square/>
      </div>
     <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <Square/>
+      <Square/>
+      <Square/>
      </div>
     </>
   )
